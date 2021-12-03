ef612984abe4f9f5f3f4d66b572ae4c1af713961
Mia <mia.bajic@firma.seznam.cz>
2021-12-03 20:05:47 +0100

    small changes

--- day1.py
+++ day1.py
@@ -2,6 +2,7 @@ import csv
 
 depth = []
 depths = []
+counter = 0 
 
 with open('./data/day1.csv', newline = '') as csvfile:
     reader = list(csv.reader(csvfile, delimiter = ' '))
@@ -9,19 +10,8 @@ with open('./data/day1.csv', newline = '') as csvfile:
     for i in reader:
         k = int(', '.join(i)) 
         depths.append(k)
-    print(len(depths))
     for n in range(1, len(depths)-1):
-        if depths[n] < depths[n - 1]:
-        # print(reader)
-            depth.append('decrease')
-        else:
-            depth.append('increase')
+        if depths[n] > depths[n - 1]:
+            counter += 1
 
-#print(depth)
-print(depth)
-
-counter = 0 
-for m in depth:
-    if m == 'increase':
-        counter += 1
 print(counter)

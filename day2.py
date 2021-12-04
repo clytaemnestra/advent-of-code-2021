cdb989929acea77429a5507e271be022b52a894d
Mia <mia.bajic@firma.seznam.cz>
2021-12-04 02:13:16 +0100

    added 2nd part of day 2

--- day2.py
+++ day2.py
@@ -10,7 +10,25 @@ for position in positions:
         horizontal += units
     elif "down" in position:
         depth += units
+    # up
     else:
         depth -= units
-
 print(horizontal * depth)    
+
+horizontal = 0
+units = 0
+aim = 0
+depth = 0
+
+for position in positions:
+    units = int(position[-1])
+    if "forward" in position:
+        horizontal += units
+        if horizontal != 0:
+            depth += aim * units
+    elif "down" in position:
+        aim += units
+    # up
+    else:
+        aim -= units
+print(horizontal * depth) 
